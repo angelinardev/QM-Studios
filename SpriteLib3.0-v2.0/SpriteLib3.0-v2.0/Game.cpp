ed1b4dcfa8f7e2898edf6d731ae1378470b01eb0
Angelina <angelinar0119@gmail.com>
2021-04-08 20:16:51 -0400

    cxbdn

--- SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/Game.cpp
+++ SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/Game.cpp
@@ -42,30 +42,30 @@ void Game::InitGame()
 	//Creates a new scene.
 	//Replace this with your own scene.
 	m_scenes.push_back(new TitleScreen("The Beyond"));
-	m_scenes.push_back(new PhysicsPlayground("Tutorial"));
-	m_scenes.push_back(new EndScreen("Game Over"));
-	m_scenes.push_back(new Cutscene1("Cutscene opening"));
-	m_scenes.push_back(new LevelOne("First level"));
-	m_scenes.push_back(new CreditScene("Credits"));
-	m_scenes.push_back(new Controls("Controls"));
-	m_scenes.push_back(new LevelTwo("Second Level"));
-	m_scenes.push_back(new Cutscene2("The End"));
-	m_scenes.push_back(new Credits2("Credits"));
+	m_scenes.push_back(new PhysicsPlayground("The Beyond"));
+	m_scenes.push_back(new EndScreen("The Beyond"));
+	m_scenes.push_back(new Cutscene1("The Beyond"));
+	m_scenes.push_back(new LevelOne("The Beyond"));
+	m_scenes.push_back(new CreditScene("The Beyond"));
+	m_scenes.push_back(new Controls("The Beyond"));
+	m_scenes.push_back(new LevelTwo("The Beyond"));
+	m_scenes.push_back(new Cutscene2("The Beyond"));
+	m_scenes.push_back(new Credits2("The Beyond"));
 	
 	//preloads all scenes
-  //m_scenes[0]->InitTexture();
-   //m_scenes[1]->InitTexture();
-	//m_scenes[2]->InitTexture();
-	//m_scenes[3]->InitTexture();
-	//m_scenes[4]->InitTexture();
-	//m_scenes[5]->InitTexture();
-	//m_scenes[6]->InitTexture();
-	//m_scenes[7]->InitTexture();
-	//m_scenes[8]->InitTexture();
+	m_scenes[0]->InitTexture();
+	m_scenes[1]->InitTexture();
+	m_scenes[2]->InitTexture();
+	m_scenes[3]->InitTexture();
+	m_scenes[4]->InitTexture();
+	m_scenes[5]->InitTexture();
+	m_scenes[6]->InitTexture();
+	m_scenes[7]->InitTexture();
+	m_scenes[8]->InitTexture();
 	m_scenes[9]->InitTexture();
 
 	//Sets active scene reference to our scene
-	m_activeScene = m_scenes[9];
+	m_activeScene = m_scenes[0];
 
 	m_activeScene->InitScene(float(BackEnd::GetWindowWidth()), float(BackEnd::GetWindowHeight()));
 
